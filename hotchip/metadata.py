316262e573a0e7a9cd5c732910b343a07c8301ae
Michael-Keith Bernard <mkbernard@opentable.com>
2015-08-19 13:20:28 -0700

    Initial commit
    Project structure generated from:
    https://github.com/seanfisk/python-project-template

---
+++ hotchip/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""HotChip
+
+Singularity command-line client and request orchestration utility.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'hotchip'
+project = 'HotChip'
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Singularity command-line client and request orchestration utility'
+authors = ['Michael-Keith Bernard']
+authors_string = ', '.join(authors)
+emails = ['mkbernard.dev@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/segfaultax/hotchip'
